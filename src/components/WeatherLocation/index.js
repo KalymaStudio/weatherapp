f4c7585d0b3d4c7e580aec72a5f7ad3fa15df47c
Natalia Menezes <60798369+KalymaStudio@users.noreply.github.com>
2020-03-22 13:40:17 -0300

    Agregado de constantes

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {SUN} from '../../constants/weathers';
 import './style.css';
+
+const data = {
+    temperature: 20,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+};
 
 const WeatherLocation = () => {
     return(
         <div className='weatherLocationCont'>
             <Location city={'Buenos aires'}/>
-            <WeatherData/>
+            <WeatherData data={data}/>
         </div>
     )
 }
