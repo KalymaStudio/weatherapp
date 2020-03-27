6374b5b08863d302358f14bc2c1bac5ccfd4d116
Natalia Menezes <60798369+KalymaStudio@users.noreply.github.com>
2020-03-26 21:06:57 -0300

    Class component,setState y click actions

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import {SUN} from '../../constants/weathers';
+import {SUN, WINDY} from '../../constants/weathers';
 import './style.css';
 /*probar git */
 
-const data = {
+const data1 = {
+    temperature: 15,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '30 m/s',
+};
+
+const data2 = {
     temperature: 20,
     weatherState: SUN,
     humidity: 10,
     wind: '10 m/s',
 };
 
-const WeatherLocation = () => {
-    return(
-        <div className='weatherLocationCont'>
-            <Location city={'Buenos aires'}/>
-            <WeatherData data={data}/>
-        </div>
-    )
+class WeatherLocation extends Component { 
+    constructor(){
+        super();
+        this.state = {
+            city: 'Buenos Aires',
+            data: data1
+        }
+    }
+    handleUpdateClick = () => {
+        this.setState ({
+            data: data2
+        });
+        console.log("Actualizado");
+    }
+    render = () => {
+        const {city,data} = this.state;
+        return (
+            <div className='weatherLocationCont'>
+                <Location city={city}/>
+                <WeatherData data={data}/>
+                <button onClick={this.handleUpdateClick} >Actualizar</button>
+            </div>
+        )
+    };
 }
 
 export default WeatherLocation;
